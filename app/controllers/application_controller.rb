6a3ba88f145813f148a5c5b4c9d749b0fc6f6d77
vagnercruz <vagnerdacruz48@gmail.com>
2024-07-09 22:37:56 -0300

    ajustes de linguagem, adicionado o locale para pt-br e en

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,18 @@
+# app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def change_language
+    locale = params[:locale]
+    if I18n.available_locales.map(&:to_s).include?(locale)
+      session[:locale] = locale
+    end
+    redirect_back(fallback_location: root_path)
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = session[:locale] || I18n.default_locale
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
       get :completed
     end
   end
+
+  get 'change_language/:locale', to: 'application#change_language', as: :change_language
   root 'books#index'
 end
